e827897547af257dd185a8a899a42ea741bd1132
balakumarks95 <balakumar.ks95@gmail.com>
2026-04-14 09:35:54 +0530

    Revert "Cognito setup"
    This reverts commit 755a563fffcecfefcd003bf1c38715d86cca70d4.

--- src/handlers/auth/triggers/verifyAuthChallenge.ts
+++
@@ -1,9 +0,0 @@
-export const handler = async (event: any) => {
-  // VerifyAuthChallenge: compare provided answer with privateChallengeParameters.answer
-  const expected = event.request.privateChallengeParameters?.answer;
-  const provided = event.request.challengeAnswer;
-
-  event.response.answerCorrect = provided && expected && provided === expected;
-
-  return event;
-};
